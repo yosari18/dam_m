0760062f85385b88ffe633697fdee4d0f91a676a
yosari18 <yosavenpac@alu.edu.gva.es>
2024-01-29 21:43:27 +0100

    entorno

---
+++ PROGRAMACION/POOBJETOS/Ejemplo/src/ejemplo/Ejemplo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author yosari
+ */
+public class Ejemplo {
+  Scanner entrada=new Scanner (System.in);
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        Persona p1=new Persona();
+        
+        System.out.println("nom :");
+    }
+    
+}
